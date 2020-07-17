6f0bfe173cea544d27d398995da3accdcb78256a
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2020-07-17 19:44:06 +0300

    init

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'reactstrap';
+import './App.scss';
+
+const App = () => {
+  return (
+    <Container>
+      init
+    </Container>
+  );
+}
+
+export default App;

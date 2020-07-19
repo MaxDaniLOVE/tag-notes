60bd65a70888885f397f893859a7a1f5af01f13d
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2020-07-19 15:43:43 +0300

    fix: fix slow updating when server not launched

--- src/services/APIService.js
+++ src/services/APIService.js
@@ -1,26 +1,40 @@
 import { staticNotes } from '../utils/constants';
 
 class APIService {
+  #isServerActive = true;
+
   getData = async () => {
     try {
       const response = await fetch('http://localhost:8080/');
       const notes = await response.json();
+
       return notes;
     } catch (error) {
+      this.#isServerActive = false;
       console.error('Static data was loaded. Please run `npm run server` command to use data from .json file');
+
       return staticNotes;
     }
   }
 
   saveData = async (data) => {
+    if (!this.#isServerActive) {
+      return console.error('It looks like you don\'t use server. Please run `npm run server` command to save data to .json file');
+    }
+
+    let response;
+
     try {
-      await fetch('http://localhost:8080/update_file', {
+      response = await fetch('http://localhost:8080/update_file', {
         method: 'POST',
         body: JSON.stringify(data),
       });
     } catch (error) {
       console.error('Can\'t update file. Try later');
     }
+
+    response = await response.json();
+    return response;
   }
 }
 

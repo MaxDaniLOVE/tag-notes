22d81ba2b0ff20f34d20568f2a3f8e367e545492
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2020-07-19 14:45:02 +0300

    feat: add write feature at server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,13 +1,30 @@
 const http = require('http');
-const notes = require('./notes');
+const fs = require('fs');
 
 const hostname = 'localhost';
 const port = 8080;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.end(JSON.stringify(notes));
+  res.setHeader('Access-Control-Allow-Origin', '*');
+});
+
+server.on('request', (req, res) => {
+  if (req.url === '/') {
+    const rawdata = fs.readFileSync(`${__dirname}/notes.json`);
+    res.end(rawdata);
+  } else {
+    let body = [];
+    req
+      .on('data', (chunk) => {
+        body.push(chunk);
+      })
+      .on('end', () => {
+        body = Buffer.concat(body).toString();
+        fs.writeFileSync(`${__dirname}/notes.json`, body);
+        res.end(JSON.stringify({ message: 'successfully updated!' }));
+      });
+  }
 });
 
 server.listen(port, hostname, () => {
